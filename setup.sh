114366bccf7fcad08a731cb3ad7437da50fe9396
deathsy <kittipong.sri@mail.kmutt.ac.th>
2019-10-17 18:11:56 +0700

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo docker run --name mongodb -d mongo:3.4
 
 echo "STARTING EXPRESS..."
 sudo docker image build -t conduit-bn -f ./backend.Dockerfile .
-sudo docker container run -p 3000:3000 \
+sudo docker container run -ditp 3000:3000 \
   -e MONGODB_URI=mongodb://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongodb):27017 \
   -e SECRET=secret \
   --name conduit-bn \
@@ -12,7 +12,7 @@ sudo docker container run -p 3000:3000 \
 echo "STARTING FRONTEND..."
 
 sudo docker image build -t conduit-fn -f ./frontend.Dockerfile .
-sudo docker container run -p 4100:80 \
+sudo docker container run -ditp 4100:80 \
   -e REACT_APP_APIENDPOINT=http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' conduit-bn):4100 \
   --name conduit-fn \
   conduit-fn
