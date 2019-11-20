ac2ee33fe61df8cf406774d05d4a60a7f22ed7f4
deathsy <kittipong.sri@mail.kmutt.ac.th>
2019-11-20 14:39:32 +0700

    remove all test

--- cypress/integration/datetime_spec.js
+++
@@ -1,8 +0,0 @@
-describe("https://www.skyscanner.co.th", function() {
-  it("should render time correctly", function() {
-    const now = new Date(2017, 11, 13).getTime();
-    cy.clock(now);
-
-    cy.visit("https://www.skyscanner.co.th");
-  });
-});
